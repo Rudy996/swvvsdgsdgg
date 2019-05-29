1b001627a818926e9a976657c17e7e4573134249
Rudy <50031661+Rudy996@users.noreply.github.com>
2019-05-29 16:00:29 +0300

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,14 @@
+menu.onclick = function myFunction() {
+	var x = document.getElementById('menu1')
+
+	if (x.className === "menu1") {
+		x.className += "responsive";
+	} else {
+		x.className = "menu1";
+	}
+
+
+
+
+}
+
